214de37fa2c0ec2b936c371b33bf41dd909548c5
guilhermerodrigues680 <guilhermerodrigues680@gmail.com>
2019-11-28 23:48:34 -0300

    Initial commit

---
+++ src/app/api.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+
+//
+import { HttpClient } from '@angular/common/http';
+
+@Injectable()
+export class ApiService {
+
+private apiRoot = 'http://localhost:8000/';
+
+  constructor(
+    private http: HttpClient,
+  ) { }
+
+  getDados() {
+    return this.http.get(this.apiRoot.concat('shopping-item/'));
+  }
+
+}
